9cfe94d7338b505b63a11b8087ccfb92a8ed977b
rsundhar247 <rsundhar247@gmail.com>
2017-11-11 18:03:37 -0500

    Added method to spell check a sentence

--- src/app/SpellCheck.java
+++ src/app/SpellCheck.java
@@ -1,9 +1,44 @@
 package app;
 
+import java.util.ArrayList;
+
 public class SpellCheck {
+	
+	static TrieUtil trie = new TrieUtil();
 
 	public static void main(String[] args) {
-		TrieUtil trie = new TrieUtil();
+		String input = "Imagination is the beginning of creation. You imagine what you desire; you will what you imagine; and at last you create what you will.";
+		trie.insert("you");
+		trie.insert("imagination");
+		trie.insert("and");
+		trie.insert("will");
+		trie.insert("what");
 		
+		spellCheck(input);
 	}
+	
+	public static void spellCheck(String input) {
+		
+		boolean isValid = false;
+		ArrayList<String> notValid = new ArrayList<String>();
+		String[] wordList = input.trim().toLowerCase().replaceAll("[^a-z ]", "").split(" ");
+		
+		for(int i=0; i<wordList.length; i++) {
+			isValid = trie.search(wordList[i]);
+			if(! isValid) {
+				notValid.add(wordList[i]);
+			}
+		}
+		
+		if(! notValid.isEmpty()) {
+			System.out.println("Invalid Words are :: ");
+			for(String word: notValid) {
+				System.out.println(word);
+			}
+		} else {
+			System.out.println("No Invalid words");
+		}
+	}
+	
+	
 }
