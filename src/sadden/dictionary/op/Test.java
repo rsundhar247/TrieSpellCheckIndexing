5a44e10cb4e719a5f12e2ce83008395d9a18bec1
luke <ll688@scarletmail.rutgers.edu>
2017-11-20 23:32:07 -0500

    Add dictionary
    we can read all the words and add the words

---
+++ src/sadden/dictionary/op/Test.java
@@ -0,0 +1,14 @@
+package sadden.dictionary.op;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Dictionary dic = new Dictionary();
+		dic.ReadDictionary();
+		dic.WriteDictionary("44434sre");
+		dic.WriteDictionary("556");
+	}
+
+}
